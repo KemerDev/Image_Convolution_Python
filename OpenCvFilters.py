10ddc0d20b0f4fac38d9a56e3667ed6e2b96e004
kemerios <kemerios10@gmail.com>
2022-03-05 21:12:18 +0100

    commit

--- OpenCvFilters.py
+++ OpenCvFilters.py
@@ -1,32 +1,38 @@
 import cv2
 import numpy as np
 
+convol_list = []
 
 def open_img():
-    img_path = "1.jpg"
-    load_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    img_path = "enemy.jpg"
+    load_img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
     return load_img
 
 def choose_filter():
     filters = []
-    edge_kernel = np.array(([-1, -1, -1],
+
+    ridge_kernel = np.array(([-1, -1, -1],
                             [-1, 8, -1],
-                            [-1, -1, -1]), dtype=int)
+                            [-1, -1, -1]), dtype=np.int32)
+
     identity_kernel = np.array(([0, 0, 0],
                                 [0, 1, 0],
-                                [0, 0, 0]), dtype=int)
+                                [0, 0, 0]), dtype=np.int32)
+
     sharpen_kernel = np.array(([0, -1, 0],
                                [-1, 5, -1],
-                               [0, -1, 0]), dtype=int)
+                               [0, -1, 0]), dtype=np.int32)
 
     boxBlur_kernel = np.array(([1, 1, 1],
                                [1, 1, 1],
-                               [1, 1, 1]), dtype=int)
-    gaussianBlur_kernel = np.array(([1, 1, 1],
-                               [1, 1, 1],
-                               [1, 1, 1]), dtype=int)
+                               [1, 1, 1]), dtype=np.int32)
 
-    filters.append(edge_kernel)
+    gaussianBlur_kernel = np.array(([1, 1, 1],
+                                    [1, 1, 1],
+                                    [1, 1, 1]), dtype=np.int32)
+
+
+    filters.append(ridge_kernel)
     filters.append(identity_kernel)
     filters.append(sharpen_kernel)
     filters.append(boxBlur_kernel)
@@ -34,25 +40,28 @@ def choose_filter():
 
     return filters
 
-def covolution(filter,img):
+# pick a kernel, convolute the img and save it as test.jpg
+def convolution(filter,img):
+    # final 1237, 1856
+    c_filter = filter[0]
 
-    #edge detection matrix RGB
-    choice = int(input("give choise for filter:"))
+    k_size = len(c_filter)
 
-    img_edge = cv2.filter2D(img, -1, filter[choice])
+    i_padded = np.pad(img, (k_size -3, k_size -3))
 
-    if(choice == 1):
-        new = cv2.cvtColor(img_edge, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("test", new)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
-    else:
-        cv2.imshow("test", img_edge)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
+    image_w, image_h = img.shape
+    
+    for row in range(image_w - 3):
+        for col in range(image_h - 3):
+            convol_list.append(np.sum(i_padded[row:k_size+row, col:k_size+col] * c_filter))
+    
+    convol_arr = np.array(convol_list, dtype=np.float32)
+    convol_arr.shape = (-1, int(image_w - 3))
+    
+    cv2.imwrite('test.jpg', convol_arr)
 
 if __name__ == "__main__":
     filter = []
     load_img = open_img()
     filter = choose_filter()
-    covolution(filter, load_img)
+    convolution(filter, load_img)
